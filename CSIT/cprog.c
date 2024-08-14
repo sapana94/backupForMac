180577b2d28fa50c46c1303d0d4cd7cb9d68683c
sapana94 <sapanathapachhetri91@gmail.com>
2024-08-14 12:47:00 +0545

    BackupAddedDattebYOO!!!Try2!

---
+++ CSIT/cprog.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    Float c,f;
+    printf("enter the temperature in centigrade\n");
+    scanf("%f",&c);
+    f=(c*1.8)+32;
+    printf("temoerature in fahrenhiteis %f\n",f);
+}
